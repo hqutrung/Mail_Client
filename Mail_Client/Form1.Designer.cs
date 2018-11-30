58a585ae46e24046b42ff76aa199e6dbeb8a89c7
Tran Anh Thang <45091241+thangta881999@users.noreply.github.com>
2018-11-30 16:20:52 +0700

    Thắng

--- Mail_Client/Form1.Designer.cs
+++ Mail_Client/Form1.Designer.cs
@@ -28,42 +28,243 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Send = new System.Windows.Forms.Panel();
-            this.Receive = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.txtAttachment = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtBody = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.Label();
+            this.txtRecipient = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rtxtReceive = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Send
+            // groupBox1
             // 
-            this.Send.AccessibleName = "";
-            this.Send.Location = new System.Drawing.Point(12, 12);
-            this.Send.Name = "Send";
-            this.Send.Size = new System.Drawing.Size(308, 482);
-            this.Send.TabIndex = 0;
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Controls.Add(this.txtAttachment);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtBody);
+            this.groupBox1.Controls.Add(this.txtSubject);
+            this.groupBox1.Controls.Add(this.txtRecipient);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Location = new System.Drawing.Point(43, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(381, 599);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Send";
             // 
-            // Receive
+            // button3
             // 
-            this.Receive.Location = new System.Drawing.Point(338, 12);
-            this.Receive.Name = "Receive";
-            this.Receive.Size = new System.Drawing.Size(369, 482);
-            this.Receive.TabIndex = 1;
+            this.button3.Location = new System.Drawing.Point(256, 233);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(44, 205);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(287, 22);
+            this.textBox7.TabIndex = 31;
+            // 
+            // txtAttachment
+            // 
+            this.txtAttachment.AutoSize = true;
+            this.txtAttachment.Location = new System.Drawing.Point(41, 185);
+            this.txtAttachment.Name = "txtAttachment";
+            this.txtAttachment.Size = new System.Drawing.Size(108, 17);
+            this.txtAttachment.TabIndex = 30;
+            this.txtAttachment.Text = "Add Attachment";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(123, 534);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 32);
+            this.button1.TabIndex = 29;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(44, 276);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(287, 239);
+            this.textBox5.TabIndex = 28;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(44, 160);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(287, 22);
+            this.textBox4.TabIndex = 27;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(44, 115);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(287, 22);
+            this.textBox3.TabIndex = 26;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 55);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(211, 22);
+            this.textBox2.TabIndex = 25;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 22);
+            this.textBox1.TabIndex = 24;
+            this.textBox1.Text = "trananhthang881999@gmail.com";
+            // 
+            // txtBody
+            // 
+            this.txtBody.AutoSize = true;
+            this.txtBody.Location = new System.Drawing.Point(41, 256);
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(44, 17);
+            this.txtBody.TabIndex = 23;
+            this.txtBody.Text = "Body:";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.AutoSize = true;
+            this.txtSubject.Location = new System.Drawing.Point(41, 140);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(59, 17);
+            this.txtSubject.TabIndex = 22;
+            this.txtSubject.Text = "Subject:";
+            // 
+            // txtRecipient
+            // 
+            this.txtRecipient.AutoSize = true;
+            this.txtRecipient.Location = new System.Drawing.Point(41, 95);
+            this.txtRecipient.Name = "txtRecipient";
+            this.txtRecipient.Size = new System.Drawing.Size(71, 17);
+            this.txtRecipient.TabIndex = 21;
+            this.txtRecipient.Text = "Recipient:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.AutoSize = true;
+            this.txtPassword.Location = new System.Drawing.Point(41, 60);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(73, 17);
+            this.txtPassword.TabIndex = 20;
+            this.txtPassword.Text = "Password:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.AutoSize = true;
+            this.txtEmail.Location = new System.Drawing.Point(41, 27);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(46, 17);
+            this.txtEmail.TabIndex = 19;
+            this.txtEmail.Text = "Email:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(177, 529);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 32);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Receive";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.rtxtReceive);
+            this.groupBox2.Location = new System.Drawing.Point(447, 9);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(500, 599);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Receive";
+            // 
+            // rtxtReceive
+            // 
+            this.rtxtReceive.Location = new System.Drawing.Point(6, 21);
+            this.rtxtReceive.Multiline = true;
+            this.rtxtReceive.Name = "rtxtReceive";
+            this.rtxtReceive.Size = new System.Drawing.Size(488, 488);
+            this.rtxtReceive.TabIndex = 16;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(719, 506);
-            this.Controls.Add(this.Receive);
-            this.Controls.Add(this.Send);
+            this.ClientSize = new System.Drawing.Size(959, 623);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel Send;
-        private System.Windows.Forms.Panel Receive;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label txtAttachment;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txtBody;
+        private System.Windows.Forms.Label txtSubject;
+        private System.Windows.Forms.Label txtRecipient;
+        private System.Windows.Forms.Label txtPassword;
+        private System.Windows.Forms.Label txtEmail;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox rtxtReceive;
     }
 }
 
