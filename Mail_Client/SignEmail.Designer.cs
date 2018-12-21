ee5beba882ca35b74636a5df20faa29207c04670
quangkhang20899 <44083074+quangkhang20899@users.noreply.github.com>
2018-12-21 15:00:31 +0700

    Khang
    30-Day Trial and Key

--- Mail_Client/SignEmail.Designer.cs
+++ Mail_Client/SignEmail.Designer.cs
@@ -39,6 +39,8 @@
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.linkCreate = new System.Windows.Forms.LinkLabel();
             this.iconGmail = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.iconGmail)).BeginInit();
             this.SuspendLayout();
@@ -57,10 +59,10 @@
             this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnNext.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNext.ForeColor = System.Drawing.Color.White;
-            this.btnNext.Location = new System.Drawing.Point(223, 234);
-            this.btnNext.Margin = new System.Windows.Forms.Padding(2);
+            this.btnNext.Location = new System.Drawing.Point(297, 288);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(118, 37);
+            this.btnNext.Size = new System.Drawing.Size(157, 46);
             this.btnNext.TabIndex = 2;
             this.btnNext.Text = "Next";
             this.btnNext.UseVisualStyleBackColor = false;
@@ -70,9 +72,10 @@
             // 
             this.gmail.AutoSize = true;
             this.gmail.Font = new System.Drawing.Font("Segoe UI Emoji", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gmail.Location = new System.Drawing.Point(162, 54);
+            this.gmail.Location = new System.Drawing.Point(216, 66);
+            this.gmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.gmail.Name = "gmail";
-            this.gmail.Size = new System.Drawing.Size(70, 26);
+            this.gmail.Size = new System.Drawing.Size(89, 32);
             this.gmail.TabIndex = 5;
             this.gmail.Text = "Sign in";
             // 
@@ -80,9 +83,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(126, 80);
+            this.label1.Location = new System.Drawing.Point(168, 98);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.Size = new System.Drawing.Size(177, 26);
             this.label1.TabIndex = 6;
             this.label1.Text = "to connect to Gmail";
             // 
@@ -98,10 +102,10 @@
             this.txtEmail.LineIdleColor = System.Drawing.Color.Gray;
             this.txtEmail.LineMouseHoverColor = System.Drawing.Color.Blue;
             this.txtEmail.LineThickness = 1;
-            this.txtEmail.Location = new System.Drawing.Point(35, 156);
-            this.txtEmail.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEmail.Location = new System.Drawing.Point(47, 192);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(306, 38);
+            this.txtEmail.Size = new System.Drawing.Size(408, 47);
             this.txtEmail.TabIndex = 7;
             this.txtEmail.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             // 
@@ -110,9 +114,10 @@
             this.lblEmail.AutoSize = true;
             this.lblEmail.Font = new System.Drawing.Font("Segoe UI Emoji", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblEmail.ForeColor = System.Drawing.Color.CornflowerBlue;
-            this.lblEmail.Location = new System.Drawing.Point(34, 132);
+            this.lblEmail.Location = new System.Drawing.Point(45, 162);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(96, 17);
+            this.lblEmail.Size = new System.Drawing.Size(126, 22);
             this.lblEmail.TabIndex = 8;
             this.lblEmail.Text = "Email or Phone";
             // 
@@ -124,9 +129,10 @@
             // 
             this.linkCreate.AutoSize = true;
             this.linkCreate.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkCreate.Location = new System.Drawing.Point(33, 251);
+            this.linkCreate.Location = new System.Drawing.Point(44, 309);
+            this.linkCreate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.linkCreate.Name = "linkCreate";
-            this.linkCreate.Size = new System.Drawing.Size(110, 20);
+            this.linkCreate.Size = new System.Drawing.Size(141, 25);
             this.linkCreate.TabIndex = 9;
             this.linkCreate.TabStop = true;
             this.linkCreate.Text = "Create Account";
@@ -135,21 +141,45 @@
             // iconGmail
             // 
             this.iconGmail.Image = global::Mail_Client.Properties.Resources.googlelogo;
-            this.iconGmail.Location = new System.Drawing.Point(135, 15);
+            this.iconGmail.Location = new System.Drawing.Point(180, 18);
+            this.iconGmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.iconGmail.Name = "iconGmail";
-            this.iconGmail.Size = new System.Drawing.Size(123, 39);
+            this.iconGmail.Size = new System.Drawing.Size(164, 48);
             this.iconGmail.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.iconGmail.TabIndex = 4;
             this.iconGmail.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 478);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 27);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(44, 436);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(390, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "You\'re using The 30-day Trial Version. Click below to Register";
+            // 
             // SignEmail
             // 
             this.AcceptButton = this.btnNext;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(370, 440);
+            this.ClientSize = new System.Drawing.Size(493, 542);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.linkCreate);
             this.Controls.Add(this.lblEmail);
             this.Controls.Add(this.txtEmail);
@@ -160,10 +190,11 @@
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "SignEmail";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.Load += new System.EventHandler(this.SignEmail_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.SignEmail_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.SignEmail_MouseMove);
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.SignEmail_MouseUp);
@@ -184,5 +215,7 @@
         private System.Windows.Forms.Label lblEmail;
         private System.Windows.Forms.ErrorProvider errorProvider1;
         private System.Windows.Forms.LinkLabel linkCreate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Mail_Client/SignEmail.cs
+++ Mail_Client/SignEmail.cs
@@ -7,6 +7,7 @@ using System.Drawing;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -72,6 +73,76 @@ namespace Mail_Client
             }
         }
 
+        static uint flag1;
+
+        [DllImport("TrialApp.dll", EntryPoint = "ReadSettingsStr", CharSet = CharSet.Ansi)]
+        static extern uint Initrial(String aKeyCode, IntPtr aHWnd);
+
+        [DllImport("TrialApp.dll", EntryPoint = "ReadSettingsRetStr", CharSet = CharSet.Ansi)]
+        static extern uint InitTrialReturn(String aKeyCode, IntPtr aHWnd);
+        [DllImport("TrialApp.dll", EntryPoint = "DisplayRegistrationStr", CharSet = CharSet.Ansi)]
+        static extern uint DisplayRegistration(String aKeyCode, IntPtr aHWnd);
+        private const string kLibraryKey = "7C75C71946421780553E18D9C34F46ED48A3518A063EBD49E7F9E926AAA0928654EDBBC955C2";
+        private static void OnInit()
+        {
+            try
+            {
+                Process process = Process.GetCurrentProcess();
+
+
+                uint ret = InitTrialReturn(kLibraryKey, process.MainWindowHandle);
+                if (ret == 0)
+                {
+                    flag1 = 1;
+                }
+
+            }
+            catch (DllNotFoundException ex)
+            {
+                MessageBox.Show(ex.ToString());
+                Process.GetCurrentProcess().Kill();
+
+            }
+            catch (Exception ex1)
+            {
+                MessageBox.Show(ex1.ToString());
+
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process process1 = Process.GetCurrentProcess();
+                DisplayRegistration(kLibraryKey, process1.MainWindowHandle);
+
+                label2.Visible = false;
+                button1.Visible = false;
+
+            }
+            catch (DllNotFoundException ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+            catch (Exception ex1)
+            {
+
+                MessageBox.Show(ex1.ToString());
+            }
+        }
+
+        private void SignEmail_Load(object sender, EventArgs e)
+        {
+            OnInit();
+            if (flag1 == 1)
+            {
+                label2.Visible = false;
+                button1.Visible = false;
+            }
+
+        }
 
         private void SignEmail_MouseUp(object sender, MouseEventArgs e)
         {
